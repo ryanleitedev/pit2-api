fd30e156e58eb38e287ba40bb5d014a1da06c366
ryanleitedev <ryanleitedev@gmail.com>
2024-05-14 13:54:07 -0300

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,26 @@
+import express from 'express'
+import routes from './routes'
+import cors from 'cors'
+
+class App {
+  public server: express.Express
+
+  constructor() {
+    this.server = express()
+
+    this.middlewares()
+    this.routes()
+  }
+
+  middlewares() {
+    this.server.use(express.json())
+    this.server.use(express.urlencoded({ extended: true }))
+    this.server.use(cors({ origin: '*', credentials: true }))
+  }
+
+  routes() {
+    this.server.use('/api', routes)
+  }
+}
+
+export default new App().server
